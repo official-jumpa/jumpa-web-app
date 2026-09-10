140ea80bd4312a2e11d08dfe22701aa78e8863d0
martinvibes <martinmachiebe21@gmail.com>
2026-09-10 11:35:44 +0100

    add sentense to empty notification tab

--- components/notifications/notifications-empty.tsx
+++ components/notifications/notifications-empty.tsx
@@ -5,7 +5,7 @@ export function NotificationsEmpty() {
   return (
     // The frame centres the bell in the 852 artboard, which is above the centre
     // of the space left under the chips.
-    <div className="flex flex-1 items-center justify-center pb-38">
+    <div className="flex flex-1 flex-col leading-4.5 text-center items-center justify-center pb-38">
       <Image
         src="/images/notifications/no-notifications.svg"
         alt="No notifications"
@@ -13,6 +13,10 @@ export function NotificationsEmpty() {
         height={202}
         className="w-[181px]"
       />
+
+      <h2 className="text-jumpa-black font-medium text-base pt-8">No notifications yet</h2>
+
+      <p className="text-jumpa-grey-300 pt-2 text-sm font-medium leading-3.5">You’re all caught up. We’ll let you know when <br /> there’s something worth your attention.</p>
     </div>
   );
 }

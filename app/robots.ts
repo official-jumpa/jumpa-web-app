11df75e56195afc2ab7099ab0dcdcbf143de9673
czDamian <olebueziedamian@gmail.com>
2026-09-21 15:06:19 +0100

    improved allowed pages

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: ['/$', '/onboarding', '/sign-in', '/sign-up'],
+        allow: ['/$', '/onboarding', '/sign-in', '/sign-up', '/_next/', '/images/', '/fonts/', '/icons/', '/manifest.webmanifest', '/robots.txt', '/sitemap.xml', '/favicon.ico', '/favicon-16x16.png', '/favicon-32x32.png', '/apple-touch-icon.png'],
         disallow: ['/account/', '/api/', '/*'],
       },
       {
